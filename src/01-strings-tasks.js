ea03a964095d83d51adef82ff490b87c433fb84e
Vlad <makrak.vladislav@yandex.ru>
2022-07-22 23:32:21 +0300

    feat: add isString solution

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -247,8 +247,8 @@ function encodeToRot13(str) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  return (Object.prototype.toString.call(value) === '[object String]');
 }
 
 
