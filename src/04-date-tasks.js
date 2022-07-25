8af290ffbe081b61e2fb79f09f7020fc873f3caa
Vlad <makrak.vladislav@yandex.ru>
2022-07-25 03:55:46 +0300

    feat: add isLeapYear solution

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -53,8 +53,16 @@ function parseDataFromIso8601(value) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = new Date(date).getFullYear();
+  if (year % 4) {
+    return false;
+  } if (year % 100) {
+    return true;
+  } if (year % 400) {
+    return false;
+  }
+  return true;
 }
 
 
